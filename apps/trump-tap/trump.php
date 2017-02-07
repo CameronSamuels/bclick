9d685a2874066cf371367c20ed17558ff170ed98
Cameron Samuels <satchmosanders@gmail.com>
2017-02-07 23:07:08 +0000

    Fixed update mysql query for trump.php

--- apps/trump-tap/trump.php
+++ apps/trump-tap/trump.php
@@ -7,7 +7,7 @@ $link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
 if (!$link) { die('Could not connect: ' . mysql_error()); }
 $db_selected = mysql_select_db(DB_NAME, $link);
 if (!$db_selected) { die('Can\'t use ' . DB_NAME . ': ' . mysql_error()); }
-$sql = "UPDATE TrumpDislikes SET Dislikes = Dislikes + 1";
+$sql = "UPDATE TrumpDislikes SET Dislikes = Dislikes + 1 WHERE name = 'game'";
 if (!mysql_query($sql)) { die('Error: ' . mysql_error()); }
 mysql_close();
 ?>
