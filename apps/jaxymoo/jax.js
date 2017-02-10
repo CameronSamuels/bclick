568b923c3f66f0af85a36360e064f755fd2d2e14
Cameron Samuels <satchmosanders@gmail.com>
2017-02-10 12:08:13 -0600

    Made JaxyMoo!!!!

---
+++ apps/jaxymoo/jax.js
@@ -0,0 +1,9 @@
+function id(id) { return document.getElementById(id); }
+var snd = new Audio('jax.m4a');
+
+
+
+// var shakeEvent = new Shake({threshold: 15});
+// shakeEvent.start();
+// window.addEventListener('shake', function(){click()}, false);
+// function stopShake() { shakeEvent.stop(); }
