827d42dea8e7add8e1683b4161c725141503ffb4
Cameron Samuels <satchmosanders@gmail.com>
2017-02-18 12:02:16 -0600

    b-Ocracy v1.1.0a0 - Made new folder for v2 before release - Added Dark Knight B to Aonarchy and Aonarchy+, added Apatosaurus to Dinotera, made divs change dynamically when orientation change

---
+++ bocracy/2/index.js
@@ -0,0 +1,30 @@
+function id(id) { return document.getElementById(id) }
+var isMobile = {
+	Android: function() { return navigator.userAgent.match(/Android/i); },
+	iOS: function() { return navigator.userAgent.match(/iPhone|iPad|iPod/i); },
+	Windows: function() { return navigator.userAgent.match(/IEMobile/i); },
+	any: function() { return (isMobile.Android() || isMobile.iOS() || isMobile.Windows()); }
+};
+
+if (!isMobile.any()) id('homescreenTip').style.display = 'none';
+if (window.navigator.standalone == true) id('homescreenTip').style.display = 'none';
+
+function indexOfMax(arr) {
+    if (arr.length === 0) return -1;
+    var max = arr[0]; var maxIndex = 0;
+    for (i = 1; i < arr.length; i++) {
+        if (arr[i] > max) { maxIndex = i; max = arr[i]; }
+    } return max;
+}
+function dynamicDivSize() {
+	var a = document.querySelectorAll('div:not(#title):not(.clear)');
+	for (i = 0; i < a.length; i++) {
+		a[i].style.padding = "0px";
+	}
+	var height = indexOfMax(a).offsetHeight;
+	for (i = 0; i < a.length; i++) {
+		a[i].style.padding = "calc(40px + " + (height / 2) + "px - " + (a[i].offsetHeight / 2) + "px) 2.5%";
+	}
+}
+window.addEventListener('orientationchange', dynamicDivSize);
+dynamicDivSize();
