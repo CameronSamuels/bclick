24c4ae3732fc1a34fae7f259b98ba36f217884b5
Cameron Samuels <satchmosanders@gmail.com>
2017-04-04 15:44:00 -0500

    Cleaned up apps directory Removed insulted, trump-tap, b-plugin, dice thrower, bnoodle and jaxy moo from apps folder

--- apps/jaxymoo/jax.js
+++
@@ -1,9 +0,0 @@
-function id(id) { return document.getElementById(id); }
-var snd = new Audio('jax.m4a');
-
-
-
-// var shakeEvent = new Shake({threshold: 15});
-// shakeEvent.start();
-// window.addEventListener('shake', function(){click()}, false);
-// function stopShake() { shakeEvent.stop(); }
