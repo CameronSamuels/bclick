98a80bd1dabd622c664210bae37d80f6d37dfba1
Cameron Samuels <satchmosanders@gmail.com>
2017-04-09 16:06:41 -0500

    Updated moving mango

--- apps/moving-mango/script.js
+++ apps/moving-mango/script.js
@@ -1,20 +1,20 @@
-var MangoClicked = false, l = [10, 50, 100, 300, 750], isMobile = {
-    Android: function() { return navigator.userAgent.match(/Android/i); },
-    iOS: function() { return navigator.userAgent.match(/iPhone|iPad|iPod/i); },
-    any: function() { return (isMobile.Android() || isMobile.iOS()); }
-};
+var MangoClicked = false, l = [10, 50, 100, 300, 750], cooldown = true;
 function get(what) { return localStorage[what] }
 function set(what, value) { localStorage[what] = value }
 function id(what) { return document.getElementById(what) }
 set('points', get("points") || 0);
 set('level', get("level") || 1);
+function log(text) {
+    var log = document.querySelector('log');
+    log.innerHTML = text; setTimeout(function(){log.innerHTML=''}, 2000);
+}
 function load() {
     id("points").innerHTML = "Points: " + get("points");
-    if (!isMobile.any()) {
+    if (!navigator.userAgent.match(/Android/i) && !navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
         id("mango").addEventListener("click", WhenClicked, false);
         document.body.addEventListener("click", BodyClicked, false);
     }
-    id("mango").setAttribute("class", "Level" + get("level"));
+    if (id("mango").getAttribute("class") != "Level" + get("level")) id("mango").setAttribute("class", "Level" + get("level"));
 }
 
 function WhenClicked() {
@@ -24,15 +24,16 @@ function WhenClicked() {
     for (i = 0; i < l.length; i++) {
         if (get("points") >= l[i] && get("points") < l[i + 1] && get("level") != i + 2) {
             set('level', i + 2);
-            alert("Level " + (get("level") - 1) + " Complete!");
-            id("mango").setAttribute("class", "Level" + get("level"));
+            log("Level " + (get("level") - 1) + " Complete!");
+            cooldown = false; setTimeout(function(){cooldown=true}, 2000);
+            if (id("mango").getAttribute("class") != "Level" + get("level")) id("mango").setAttribute("class", "Level" + get("level"));
             break;
         }
     }
 }
 
 function BodyClicked() {
-    if (MangoClicked == false) {
+    if (MangoClicked == false && cooldown == true) {
         if (get("points") >= 15) {
             set('points', parseFloat(get("points")) - 15);
             id("points").innerHTML = "Points: " + get("points");
@@ -40,7 +41,7 @@ function BodyClicked() {
             set('points', 0);
             set('level', 1);
             load();
-            alert("GAME OVER!!");
+            log("GAME OVER!!");
         }
     }
     else MangoClicked = false;
